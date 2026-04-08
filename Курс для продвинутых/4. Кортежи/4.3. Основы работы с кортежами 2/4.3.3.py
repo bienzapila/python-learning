fccf6648ad4c620dc927d06a25c90268261cb6b8
bienzapila <antonzhukovskiyforreal@gmail.com>
2026-04-08 17:28:51 +0300

    Created a script for Tribonacci sequence

--- Курс для продвинутых/4. Кортежи/4.3. Основы работы с кортежами 2/4.3.3.py
+++ Курс для продвинутых/4. Кортежи/4.3. Основы работы с кортежами 2/4.3.3.py
@@ -3,7 +3,7 @@ n = int(input())
 f1, f2, f3 = 1, 1, 1
 ans = [1, 1, 1]
 for i in range(n):
-    f1, f2, f2 = f2, f3, f1 + f2 + f3
-    ans.append(f1)
+    f1, f2, f3 = f2, f3, f1 + f2 + f3
+    ans.append(f3)
 
-print(*ans)
+print(*ans[:-3])
