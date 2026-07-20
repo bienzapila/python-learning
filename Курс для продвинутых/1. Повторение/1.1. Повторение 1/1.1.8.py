e25de8a16cb01a8ff9b6aec8c5d32d1e06366233
bienzapila <zhukouskianton@gmail.com>
2026-07-20 13:37:16 +0300

    Tried to solve the Josephus problem

--- Курс для продвинутых/1. Повторение/1.1. Повторение 1/1.1.8.py
+++ Курс для продвинутых/1. Повторение/1.1. Повторение 1/1.1.8.py
@@ -1,5 +1,25 @@
 n, k = int(input()), int(input())
-lst = list(range(1, n + 1))
+lst = [i for i in range(1, n + 1)]
 
-while len(lst) > 1:
-    temp = lst.pop(k-1)
+while len(lst) >= k:
+    lst = lst[k:]+(lst[:k-1])
+    print(lst)
+print()
+while len(lst) > 2:
+    m = k - len(lst)
+    lst += lst[:m]
+    key = lst[:-1]
+    if all(map(lambda x: x == key, lst)):
+        print(key)
+        break
+    while key in lst:
+        lst.remove(key)
+    tlst = []
+    for i in range(len(lst)):
+        if lst[i] not in tlst:
+            tlst.append(lst[i])
+    lst = tlst
+    print(f'temp {tlst}')
+    print(lst)
+else:
+    print(lst[0])
